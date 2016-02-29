a2cac030d16dad5b6584f6fa669bbffe08764d27
Dolan Miu <dolan_miu@hotmail.com>
2016-02-29 22:58:01 +0000

    gulp env support

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -5,7 +5,11 @@ gulp.task('default', function() {
 
 });
 
-gulp.task('serve', function () {
-    serve.run(['build/app.js']);
-    gulp.watch(['app.js', 'build/**/*.js'], [serve.run]);
+gulp.task('serve', function() {
+  env({
+    file: '.env.json'
+  });
+
+  serve.run(['build/app.js']);
+  gulp.watch(['app.js', 'build/**/*.js'], [serve.run]);
 });
